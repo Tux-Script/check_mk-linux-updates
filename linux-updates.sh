15a7f9d43b8b64f3c14a571f6122071840f66ebd
Tux-Script <61803739+Tux-Script@users.noreply.github.com>
2020-03-05 06:30:37 +0100

    add github repo

--- linux-updates.sh
+++ linux-updates.sh
@@ -7,6 +7,7 @@
 # Version 1.0
 # Script by Dipl.-Inf. Christoph Pregla
 # License: GNU GPL v3
+# https://github.com/Tux-Script/check_mk-linux-updates
 
 ###########################################
 ### Declaration var / const /command	###
